126f93644bd1eaa44c843b879e1e9475984df305
Filipe Oliveira <filipeoliveira084@gmail.com>
2022-04-04 19:35:59 -0300

    master

--- src/Pages/Table/Table.js
+++ src/Pages/Table/Table.js
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useState} from 'react';
+import AddAsset from '../../Components/AddAsset/AddAsset';
 import './Table.css'
 
 function Table(){
-
+	const [isAddModalVisible, setIsAddModalVisible] = useState(false)
 
     return(
         <>
@@ -84,7 +85,7 @@ function Table(){
 							</tr>
 
 							<tr class="tableRow">
-                               <td class="tableColumn" >BITCOIN</td>
+                               <td class="tableColumn" >ETHEREUM</td>
 								<td class="tableColumn" >COMPRA</td>
 								<td class="tableColumn" >10</td>
 								<td class="tableColumn" >10</td>
@@ -93,6 +94,56 @@ function Table(){
 							</tr>
 						</tbody>
 					</table>
+					<button className='addTableAssetBtn' onClick={()=>{setIsAddModalVisible(true)}}>Adicionar novo ativo</button> 
+					
+				</div>
+
+				{isAddModalVisible ? 
+                    <AddAsset
+                    onClose={()=>setIsAddModalVisible(false)}/> 
+                    : 
+                    null
+                }
+				
+				<div className='widgetAssetInfo'>
+					<img className='widgetAssetInfo--image'></img>
+					<div className='widgetAssetInfo--values'>
+						<h4>PRICE</h4>
+						<h4 className='widgetAssetInfo--values--variation'>VARIATION</h4>
+					</div>
+					<div className='widgetAssetInfo--others'>
+						<table>
+							<tbody>
+								<tr>
+									<td>MARKET CAP RANK</td>
+									<td>#1</td>
+								</tr>
+								<tr>
+									<td>MARKET CAP</td>
+									<td>R$804,000,000,000</td>
+								</tr>
+								<tr>
+									<td>24H VOLUME</td>
+									<td>R$14,000,000,000</td>
+								</tr>
+								<tr>
+									<td>24H HIGH/LOW</td>
+									<td>R$43,030 / R$41,191</td>
+								</tr>
+							</tbody>
+						</table>
+					</div>
+				</div>
+
+				<div className='widgetAssetConversor'>
+					<div className='widgetAssetConversor--asset'>
+						<img></img>
+						<input></input>
+					</div>
+					<div className='widgetAssetConversor--currency'>
+						<h3>BRL</h3>
+						<input></input>
+					</div>
 				</div>
         </>
     )
